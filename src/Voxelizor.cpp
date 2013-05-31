c5b5445a4c63d0c9424d27d2d91ac98cc4be75dc
= <luke.metz@students.olin.edu>
2013-05-31 17:13:51 +0000

    monster of a multithreading for voxel population

--- src/Voxelizor.cpp
+++ src/Voxelizor.cpp
@@ -3,6 +3,9 @@
 #include <OgreManualObject.h>
 #include <OgreRoot.h>
 #include <algorithm>
+#include <thread>
+#include <future>
+#include <mutex>
 
 #include "ExtractionController.hpp"
 #include "ModifiedMarchingCubesSurfaceExtractor.h"
@@ -40,6 +43,7 @@ void Voxelizor::fill()
         Vec3F(64, 64, 64),
         Vec3F(110, 5, 50)));
 
+
   for (int z = 0; z < voxel_data->getDepth(); z++) {
     for (int y = 0; y < voxel_data->getHeight(); y++) {
       for (int x = 0; x < voxel_data->getWidth(); x++) {
@@ -54,6 +58,7 @@ void Voxelizor::fill()
 
 void Voxelizor::make_wall()
 {
+  std::cout << "starting fill" << std::endl;
   VoxelFunction combine;
   combine.addFunction(FunctionLibrary::clearAll());
   
@@ -92,13 +97,118 @@ void Voxelizor::make_wall()
         Vec3F(.5, .5, .5))); //frequency
 
   combine.addFunction(FunctionLibrary::clampColor());
-  //Fix me, missing all dem caches. Iterate over 32x32 blocks
-  for (int z = 0; z < voxel_data->getDepth(); z++) {
-    for (int y = 0; y < voxel_data->getHeight(); y++) {
-      for (int x = 0; x < voxel_data->getWidth(); x++) {
-        voxel_data->setVoxelAt(x, y, z, combine.execute(x,y,z));
+  int workSize = 32*32*32;
+  std::vector<Vec3I> remaining_offsets;
+  for (int z = 0; z < voxel_data->getDepth(); z+=32) {
+    for (int y = 0; y < voxel_data->getHeight(); y+=32) {
+      for (int x = 0; x < voxel_data->getWidth(); x+=32) {
+        remaining_offsets.push_back(Vec3I(x,y,z));
+        
+        /*for (int xx = 0; xx < 32; ++xx) {
+          for (int yy = 0; yy < 32; ++yy) {
+            for (int zz = 0; zz < 32; ++zz) {
+
+              voxel_data->setVoxelAt(x+xx, y+yy, z+zz, combine.execute(x+xx,y+yy,z+zz));
+            }
+          }
+        }*/
       }
     }
+  }
+
+  int num_tasks = 6;
+  std::vector<std::vector<Voxel>> temps;
+  std::mutex done_mutex;
+  enum codes {
+    k_finished_task = 0,
+    k_running = 1,
+    k_stopped = 2
+  };
+  std::vector<int> done; //Store status codes
+  std::vector<std::thread> threads;
+  std::vector<Vec3I> on_offset;
+
+  auto function = [&temps, &combine, &done_mutex, &done](int thread_index, Vec3I offset) {
+    std::cout << "Thread "<< thread_index << "Running" << std::endl;
+    std::cout << "On offset (" << offset.getX() <<"," << offset.getY() <<","<<offset.getZ()<< ")" <<std::endl;
+    
+    for (auto i=temps[thread_index].begin(); i != temps[thread_index].end(); ++i) {
+      int loc = i - temps[thread_index].begin();
+      int x = loc%32;
+      int y = (loc%(32*32)) / 32;
+      int z = loc/32/32;
+      combine.execute(offset.getX()+x, offset.getY()+y, offset.getZ()+z, *i);
+    }
+    std::cout << "Thread " << thread_index << "Done" << std::endl;
+    
+    done_mutex.lock();
+    done[thread_index] = k_finished_task;
+    done_mutex.unlock();
+  };
+
+
+  std::cout << "b1" << std::endl; 
+  for (int i=0; i < num_tasks; ++i) {
+    done_mutex.lock();
+    done.push_back(k_running);
+    done_mutex.unlock();
+    
+    temps.push_back(std::vector<Voxel>());// workSize, Voxel(1., Vec3F(1,1,1))));
+    for (int j =0; j < workSize; j++) {
+      temps[i].push_back(Voxel());
+    }
+
+    Vec3I offset = remaining_offsets.back();
+    remaining_offsets.pop_back();
+    on_offset.push_back(offset);
+
+    auto bind_func = std::bind(function, i, offset);
+    std::cout << "Thread " << i << " Started" << std::endl;
+    
+    threads.push_back(std::thread(bind_func));
+  }
+
+  while (!std::all_of(done.begin(), done.end(), [](int f) {return f == k_stopped;})) {
+
+    auto found = std::find(done.begin(), done.end(), k_finished_task);
+    if (found == done.end()) {
+      //Still running
+      continue;
+    }
+    int i = found - done.begin();
+    threads[i].join();
+    Vec3I cur_offset = on_offset[i]; 
+    for (int loc=0; loc < workSize; ++loc) {
+      int x = loc%32;
+      int y = (loc%(32*32)) / 32;
+      int z = loc/32/32;
+      Voxel v = temps[i][loc];
+      voxel_data->setVoxelAt(cur_offset.getX() + x, cur_offset.getY() + y, cur_offset.getZ() + z, v);
+    }
+    
+
+    if (!remaining_offsets.empty()) {
+      Vec3I offset = remaining_offsets.back();
+      remaining_offsets.pop_back();
+      on_offset[i] = offset;
+      
+      auto bind_func = std::bind(function, i, offset);
+      done_mutex.lock();
+      done[i] = k_running;
+      done_mutex.unlock();
+      
+      std::cout << "Restarted Thread " << i << " Started" << std::endl;
+      threads[i] = std::thread(bind_func);
+    } else {
+      std::cout << "Stopped thread" << i << std::endl;
+      done_mutex.lock();
+      done[i] = k_stopped;
+      done_mutex.unlock();
+    }
+  }
+  
+  for (auto temp : temps) {
+    std::cout << temp[0].getDensity() << temp[199].getDensity() << std::endl;
   }
 
   std::cout << "Size is (mb): " << voxel_data->calculateSizeInBytes()/1000000.0
@@ -107,6 +217,7 @@ void Voxelizor::make_wall()
 
 void Voxelizor::to_ogre_mesh(Ogre::SceneManager &mgr)
 {
+  std::cout << "Making mesh" << std::endl;
   Ogre::ManualObject *ogre_mesh;
   ogre_mesh = mgr.createManualObject("name");
   ogre_mesh->setDynamic(true);
